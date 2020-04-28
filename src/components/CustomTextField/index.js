99c0215700fab51a0b16c37fbc111aa86b470d2b
Sabine Robart <sabine.robart@protonmail.com>
2020-04-28 14:41:00 +0200

    Update the custom inputs style + stories

--- src/components/CustomTextField/index.js
+++ src/components/CustomTextField/index.js
@@ -2,25 +2,34 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
-import clsx from 'clsx';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import IconButton from '@material-ui/core/IconButton';
 import { Visibility, VisibilityOff } from '@material-ui/icons';
 import FilledInput from '@material-ui/core/FilledInput';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import Box from '@material-ui/core/Box';
 import styles from './styles';
 
-export const CustomTextField = ({ error, errorMessage, ...props }) => {
+export const CustomTextField = ({ label, placeholder, type, error, helperText, ...props }) => {
   const classes = styles();
   return (
-    <div style={{ height: '90px' }}>
-      <TextField variant="filled" error={error} InputProps={{ classes, disableUnderline: true }} {...props} />
-    </div>
+    <Box>
+      <InputLabel className={classes.label}>{label}*</InputLabel >
+      <FilledInput
+        type={type}
+        placeholder={placeholder}
+        fullWidth
+        error={error}
+        classes={{ root: classes.root, focused: classes.focused }}
+        disableUnderline
+        {...props}
+      />
+      <FormHelperText error={error}>{helperText}</FormHelperText>
+    </Box >
   );
 };
 
-export const CustomPasswordField = ({
-  label, error, helperText, ...props
+export const CustomPasswordField = ({ label, placeholder, error, helperText, ...props
 }) => {
   const classes = styles();
   const [values, setValues] = React.useState({
@@ -43,34 +52,35 @@ export const CustomPasswordField = ({
     event.preventDefault();
   };
   return (
-    <div style={{ height: '90px' }}>
-      <FormControl variant="filled">
-        <InputLabel error={error} htmlFor="filled-adornment-password">{label}</InputLabel>
-        <FilledInput
-          classes={{ root: classes.root, focused: classes.focused }}
-          id="filled-adornment-password"
-          type={values.showPassword ? 'text' : 'password'}
-          value={values.password}
-          onChange={handleChange('password')}
-          disableUnderline
-          error={error}
-          {...props}
-          endAdornment={(
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {values.showPassword ? <Visibility /> : <VisibilityOff />}
-              </IconButton>
-            </InputAdornment>
-          )}
-        />
-        <FormHelperText error={error} htmlFor="filled-adornment-password">{helperText}</FormHelperText>
-      </FormControl>
-    </div>
+    <Box>
+      <InputLabel error={error} htmlFor="filled-adornment-password" className={classes.label}>{label}*</InputLabel>
+      <FilledInput
+        classes={{ root: classes.root, focused: classes.focused }}
+        id="filled-adornment-password"
+        placeholder={placeholder}
+        fullWidth
+        type={values.showPassword ? 'text' : 'password'}
+        value={values.password}
+        onChange={handleChange('password')}
+        disableUnderline
+        error={error}
+        {...props}
+        endAdornment={(
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              edge="end"
+              color="secondary"
+            >
+              {values.showPassword ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+        )}
+      />
+      <FormHelperText error={error} htmlFor="filled-adornment-password">{helperText}</FormHelperText>
+    </Box>
   );
 };
 
--- src/stories/components/TextField.stories.js
+++ src/stories/components/TextField.stories.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import ProviderWrapper from '../../utils/Provider';
-import CustomTextField, {CustomPasswordField} from "../../components/CustomTextField";
+import CustomTextField, { CustomPasswordField } from "../../components/CustomTextField";
 
 export default {
   title: 'Components|TextField',
   component: CustomTextField,
   parameters: {
     backgrounds: [
-      { name: 'colored-theme', value: '#26a69a', default: true },
+      { name: 'colored-theme', value: '#162217', default: true },
     ]
   },
 };
 
 export const withDefault = () => (
   ProviderWrapper(
-    <CustomTextField label="Email" />
+    <CustomTextField label="Email" placeholder="Votre email" />
   )
 );
 
 export const withErrorMessage = () => (
   ProviderWrapper(
-    <CustomTextField label="Email" error helperText="Veuillez entrer votre adresse email" />
+    <CustomTextField label="Email" placeholder="Votre email" error helperText="Veuillez entrer votre adresse email" />
   )
 );
 
 export const withPassword = () => (
   ProviderWrapper(
-    <CustomPasswordField label={"Mot de passe"}/>
+    <CustomPasswordField label="Mot de passe" placeholder="Votre mot de passe" />
   )
 );
