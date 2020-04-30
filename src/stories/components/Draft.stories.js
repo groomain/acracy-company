f35ee62b36b19516211d84760c4cd3435ba73af1
Sabine Robart <sabine.robart@protonmail.com>
2020-04-30 17:20:37 +0200

    Add the drafts stories

---
+++ src/stories/components/Draft.stories.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import ProviderWrapper from '../../utils/Provider';
+import Draft from '../../components/Drafts/Draft';
+import Drafts from '../../components/Drafts/DraftsWrapper';
+
+export default {
+  title: 'Components|Drafts',
+  component: Drafts,
+  parameters: {
+    backgrounds: [
+      { name: 'colored-theme', value: '#283028', default: true }
+    ]
+  },
+};
+
+const styles = {
+  padding: '3rem',
+}
+
+const drafts = [{
+  status: 'Status de brief',
+  progress: '30%',
+  date: '22.04',
+  time: '10h12',
+  target: 'Web design',
+  new: true
+}, {
+  status: 'Status de brief',
+  progress: '70%',
+  date: '21.04',
+  time: '12h09',
+  title: 'Mon titre de brief très long sur deux lignes',
+  target: 'Web design',
+  new: false
+}];
+
+const draft = {
+  status: 'Status de brief',
+  progress: '30%',
+  date: '21.04',
+  time: '12h09',
+  target: 'Web design',
+  new: true
+};
+
+export const withDefault = () => (
+  ProviderWrapper(
+    <div style={styles}>
+      <Draft draft={draft} />
+    </div>
+  )
+);
+
+export const withList = () => (
+  ProviderWrapper(
+    <div style={styles}>
+      <Drafts drafts={drafts}>
+        <Draft draft={draft} />
+      </Drafts>
+    </div>
+  )
+);
+
+const noDrafts = [];
+
+export const withEmptyList = () => (
+  ProviderWrapper(
+    <div style={styles}>
+      <Drafts drafts={noDrafts} />
+    </div>
+  )
+);
+
+export const withLoading = () => (
+  ProviderWrapper(
+    <div style={styles}>
+      <Drafts drafts={noDrafts} loading />
+    </div>
+  )
+);
