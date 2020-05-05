21228a30236dbbfa67a866b8440b572662afd109
Emilie Boeglen <anaporia@protonmail.com>
2020-05-05 15:40:32 +0200

    [~] First tile of the signup component done.

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { CustomButton } from '../Button/';
-import CustomTextField, { CustomPasswordField } from "../Inputs/CustomTextField";
-import Grid from '@material-ui/core/Grid';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepButton from '@material-ui/core/StepButton';
-import Typography from "@material-ui/core/Typography";
+import CustomTextField from "../Inputs/CustomTextField";
+import CustomSelect from "../Inputs/CustomSelect";
+import CustomNavLink from "../CustomNavLink";
+import { Typography, Grid, Stepper, Step, StepButton, Box } from "@material-ui/core";
 import styles from '../../utils/styles';
 
 const SignUpForm = (props) => {
@@ -29,88 +27,148 @@ const SignUpForm = (props) => {
   const setPersonnalInfos = () => {
     return (
       <>
-        <Typography className={classes.text} >{t('createAccount')}</Typography>
+        <Typography variant={"h1"} >{t('createAccount')}</Typography>
         <br />
-        <Typography className={classes.textSmall} >{t('alreadyHaveAccount')} &nbsp;
-            <span className={`${classes.textSmallYellow} ${classes.textLink}`}> {t('loginLinkMsg')}</span>
+        <br />
+        <Typography variant={"h4"} >{t('alreadyHaveAccount')} &nbsp;
+            <span> <CustomNavLink to="/login" text={t('loginLinkMsg')} theme='yellowLink'></CustomNavLink></span>
         </Typography>
         <br />
-        <Typography className={classes.textSmallYellow} >{t('accountCreation')}</Typography>
-        <Typography className={classes.text} >{t('personnalInfos')}</Typography>
-        <Typography className={classes.textSmall} >{t('companyName')}*</Typography>
-        <CustomTextField
-          name="companyName"
-          type="companyName"
-          value={companyName}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          label={t('companyNameLabel')}
-          error={!!touched.companyName && !!errors.companyName}
-          helperText={touched.companyName && errors.companyName ? t('companyNameRequired') : ''}
-        />
-        <Typography className={classes.textSmall} >{t('firstName')}*</Typography>
-        <CustomTextField
-          name="firstName"
-          type="firstName"
-          value={firstName}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          label={t('firstNameLabel')}
-          error={!!touched.firstName && !!errors.firstName}
-          helperText={touched.firstName && errors.firstName ? t('firstNameRequired') : ''}
-        />
-        <Typography className={classes.textSmall} >{t('lastName')}*</Typography>
-        <CustomTextField
-          name="lastName"
-          type="lastName"
-          value={lastName}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          label={t('lastNameLabel')}
-          error={!!touched.lastName && !!errors.lastName}
-          helperText={touched.lastName && errors.lastName ? t('lastNameRequired') : ''}
-        />
-        <Typography className={classes.textSmall} >{t('role')}*</Typography>
-        <CustomTextField
-          name="role"
-          type="role"
-          value={role}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          label={t('roleLabel')}
-          error={!!touched.role && !!errors.role}
-          helperText={touched.role && errors.role ? t('roleRequired') : ''}
-        />
-        <Typography className={classes.textSmall} >{t('emailPro')}*</Typography>
-        <CustomTextField
-          name="email"
-          type="email"
-          value={email}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          label={t('emailLabel')}
-          error={!!touched.email && !!errors.email}
-          helperText={touched.email && errors.email ? t('emailRequired') : ''}
-        />
-        <Typography className={classes.textSmall} >{t('phoneNumber')}*</Typography>
-        <CustomTextField
-          name="phoneNumber"
-          type="phoneNumber"
-          value={phoneNumber}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          label={t('phoneNumberLabel')}
-          error={!!touched.phoneNumber && !!errors.phoneNumber}
-          helperText={touched.phoneNumber && errors.phoneNumber ? t('emailRequired') : ''}
-        />
-        <Typography variant={'subtitle2'}>{signupErrorMessage}</Typography>
+        <br />
+        <Typography variant={"h2"} >{t('accountCreation')}</Typography>
+        <Typography variant={"h1"} >{t('personnalInfos')}</Typography>
+        <br />
+        <br />
+
+        <Grid container>
+          <Grid item xs={12}>
+            <CustomTextField
+              name="companyName"
+              type="companyName"
+              value={companyName}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              placeholder={t('companyNameLabel')}
+              label={t('companyName') + '*'}
+              error={!!touched.companyName && !!errors.companyName}
+              helperText={touched.companyName && errors.companyName ? t('companyNameRequired') : ''}
+            />
+          </Grid>
+
+          <Grid container item spacing={2}>
+            <Grid item xs={6}>
+              <CustomTextField
+                name="firstName"
+                type="firstName"
+                value={firstName}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                label={t('firstName') + '*'}
+                placeholder={t('firstNameLabel')}
+                error={!!touched.firstName && !!errors.firstName}
+                helperText={touched.firstName && errors.firstName ? t('firstNameRequired') : ''}
+              />
+            </Grid>
+
+            <Grid item xs={6}>
+              <CustomTextField
+                name="lastName"
+                type="lastName"
+                value={lastName}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                label={t('lastName') + '*'}
+                placeholder={t('lastNameLabel')}
+                error={!!touched.lastName && !!errors.lastName}
+                helperText={touched.lastName && errors.lastName ? t('lastNameRequired') : ''}
+              />
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <CustomTextField
+              name="role"
+              type="role"
+              value={role}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              label={t('role') + '*'}
+              placeholder={t('roleLabel')}
+              error={!!touched.role && !!errors.role}
+              helperText={touched.role && errors.role ? t('roleRequired') : ''}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <CustomTextField
+              name="email"
+              type="email"
+              value={email}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              placeholder={t('emailLabel')}
+              label={t('emailPro') + '*'}
+              error={!!touched.email && !!errors.email}
+              helperText={touched.email && errors.email ? t('emailRequired') : ''}
+            />
+          </Grid>
+
+          <Grid container item direction='column'>
+
+            <Box>
+              <Typography variant={'body1'} >{t('phoneNumber') + '*'}</Typography>
+            </Box>
+            <Grid container spacing={2}>
+
+              <Grid item xs={5}>
+                <CustomSelect></CustomSelect>
+              </Grid>
+
+              <Grid item xs={7}>
+                <CustomTextField
+                  name="phoneNumber"
+                  type="phoneNumber"
+                  value={phoneNumber}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  placeholder={t('phoneNumberLabel')}
+                  label={' '}
+                  error={!!touched.phoneNumber && !!errors.phoneNumber}
+                  helperText={touched.phoneNumber && errors.phoneNumber ? t('emailRequired') : ''}
+                />
+              </Grid>
+            </Grid>
+
+            <Typography variant={'subtitle2'}>{signupErrorMessage}</Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container justify='flex-end'>
+          <CustomButton
+            type="button"
+            theme="filledButton"
+            handleClick={() => handleStep(1)}
+            loading={signupLoading}
+            title={t('nextButton')}
+          >
+          </CustomButton>
+        </Grid>
+
       </>
     );
   }
 
   const setPassword = () => {
     return (
-      <Typography>it works?  HEY IT WOOOORKS !!!</Typography>
+      <>
+        <Typography>it works?  HEY IT WOOOORKS !!!</Typography>
+        <CustomButton
+          type="submit"
+          handleClick={() => handleSubmit({ email, password })}
+          loading={signupLoading}
+          title={t('nextButton')}
+        >
+        </CustomButton>
+      </>
     )
   }
 
@@ -133,37 +191,33 @@ const SignUpForm = (props) => {
     setActiveStep(step);
   };
 
+  {/* <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="left"
+    > */}
+  {/* </Grid> */ }
   return (
-    <>
-      {/* <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="left"
-      > */}
-      <Grid item xs={5} >
+    <Grid item className={classes.formGridItem}>
+      <Stepper nonLinear activeStep={activeStep} className={classes.stepper}>
+        {steps.map((label, index) => (
+          <Step key={label} className={classes.step}>
+            <StepButton onClick={handleStep(index)} >
 
-        <Stepper nonLinear activeStep={activeStep}>
-          {steps.map((label, index) => (
-            <Step key={label} classname={classes.stepper}>
-              <StepButton onClick={handleStep(index)} >
+              <Typography>
                 {label}
-              </StepButton>
+              </Typography>
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
+      <br />
+      <br />
+      <br />
+      {getStepContent(activeStep)}
 
-            </Step>
-          ))}
-        </Stepper>
-        {getStepContent(activeStep)}
-        <CustomButton
-          type="submit"
-          handleClick={() => handleSubmit({ email, password })}
-          loading={signupLoading}
-          title={activeStep === 0 ? t('nextButton') : t('signUpSubmit')}
-        >
-        </CustomButton>
-      </Grid>
-      {/* </Grid> */}
-    </>
+    </Grid>
   )
 };
 
