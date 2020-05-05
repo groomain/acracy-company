1dec141f7b1b99427bd863651ef4e683c3a5cae6
Emilie Boeglen <anaporia@protonmail.com>
2020-05-05 15:28:34 +0200

    [~] Add theme prop to customnavlink component and create a new thme for yellow underlined anchores.

--- src/components/CustomNavLink/styles.js
+++ src/components/CustomNavLink/styles.js
@@ -3,10 +3,18 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 export default makeStyles(theme => ({
   navLink: {
     fontSize: 19,
-    color: '#fff',
+    color: theme.palette.secondary.main,
     textDecoration: 'none',
     '&:hover': {
       color: '#e4e4e4',
     },
   },
+  yellowLink: {
+    fontsize: 17,
+    color: theme.palette.primary.main,
+    textDecoration: 'underline',
+    '&:hover': {
+      color: theme.palette.primary.bright,
+    },
+  }
 }));
