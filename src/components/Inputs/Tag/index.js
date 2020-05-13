fab51eca0d9db998e8bb53acfeb952b7b507d231
Sabine Robart <sabine.robart@protonmail.com>
2020-05-13 11:16:04 +0200

    Move styles to a styles file

--- src/components/Inputs/Tag/index.js
+++ src/components/Inputs/Tag/index.js
@@ -2,57 +2,10 @@ import React from 'react';
 import { Checkbox } from '@material-ui/core';
 
 import clsx from "clsx";
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import { withStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    '& span': {
-      textAlign: 'left',
-      padding: '.5rem 1rem',
-      fontSize: 14,
-      fontFamily: "Basier Regular",
-      color: theme.palette.secondary.medium,
-      border: `2px solid transparent`,
-    },
-    '&:hover': {
-      backgroundColor: 'transparent',
-    },
-    '&:active': {
-      marginTop: 2
-    }
-  },
-  icon: {
-    borderRadius: 50,
-    backgroundColor: theme.palette.secondary.main,
-    '$root.Mui-focusVisible &': {
-      border: `2px solid ${theme.palette.primary.main}`,
-    },
-    'input:hover ~ &': {
-      backgroundColor: theme.palette.primary.main,
-    },
-    'input:disabled ~ &': {
-      boxShadow: 'none',
-      color: 'inherit',
-      background: theme.palette.secondary.main,
-    }
-  },
-  input: {
-    textAlign: 'left'
-  },
-  checkedIcon: {
-    backgroundColor: theme.palette.primary.main,
-    '&:before': {
-      display: 'block',
-    },
-    'input:hover ~ &': {
-      backgroundColor: theme.palette.primary.main,
-    },
-  }
-}));
+import styles from './styles';
 
 const StyledCheckbox = ({ title, ...props }) => {
-  const classes = useStyles();
+  const classes = styles();
 
   return (
     <Checkbox
---
+++ src/components/Inputs/Tag/styles.js
@@ -0,0 +1,47 @@
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+export default makeStyles(theme => ({
+  root: {
+    '& span': {
+      textAlign: 'left',
+      padding: '.5rem 1rem',
+      fontSize: 14,
+      fontFamily: "Basier Regular",
+      color: theme.palette.secondary.medium,
+      border: `2px solid transparent`,
+    },
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+    '&:active': {
+      marginTop: 2
+    }
+  },
+  icon: {
+    borderRadius: 50,
+    backgroundColor: theme.palette.secondary.main,
+    '$root.Mui-focusVisible &': {
+      border: `2px solid ${theme.palette.primary.main}`,
+    },
+    'input:hover ~ &': {
+      backgroundColor: theme.palette.primary.main,
+    },
+    'input:disabled ~ &': {
+      boxShadow: 'none',
+      color: 'inherit',
+      background: theme.palette.secondary.main,
+    }
+  },
+  input: {
+    textAlign: 'left'
+  },
+  checkedIcon: {
+    backgroundColor: theme.palette.primary.main,
+    '&:before': {
+      display: 'block',
+    },
+    'input:hover ~ &': {
+      backgroundColor: theme.palette.primary.main,
+    },
+  }
+}));
