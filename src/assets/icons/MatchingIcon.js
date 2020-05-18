f52fa395dd4c24b052db418543226e20bea03201
Sabine Robart <sabine.robart@protonmail.com>
2020-05-18 14:14:37 +0200

    Rename icons props to avoid console errors

--- src/assets/icons/MatchingIcon.js
+++ src/assets/icons/MatchingIcon.js
@@ -1,15 +1,15 @@
 import SvgIcon from "@material-ui/core/SvgIcon";
 import React from "react";
 
-export const MatchingIcon  = ({...props}) => {
-    return (
-        <SvgIcon {...props} >
-            <g fill="none" fill-rule="evenodd" stroke="#ECF805" stroke-linecap="round" stroke-width="2">
-                <path d="M3 6.015V10c0 3.866 3.134 7 7 7" transform="translate(1 1)"/>
-                <path d="M1 6L1 10 5 10" transform="translate(1 1) rotate(135 3 8)"/>
-                <path d="M17.015 10.992V7.008c0-3.866-3.134-7-7-7" transform="translate(1 1)"/>
-                <path d="M15.015 7.008L15.015 11.008 19.015 11.008" transform="translate(1 1) rotate(-45 17.015 9.008)"/>
-            </g>
-        </SvgIcon>
-    );
+export const MatchingIcon = ({ ...props }) => {
+  return (
+    <SvgIcon {...props} >
+      <g fill="none" fillRule="evenodd" stroke="#ECF805" strokeLinecap="round" strokeWidth="2">
+        <path d="M3 6.015V10c0 3.866 3.134 7 7 7" transform="translate(1 1)" />
+        <path d="M1 6L1 10 5 10" transform="translate(1 1) rotate(135 3 8)" />
+        <path d="M17.015 10.992V7.008c0-3.866-3.134-7-7-7" transform="translate(1 1)" />
+        <path d="M15.015 7.008L15.015 11.008 19.015 11.008" transform="translate(1 1) rotate(-45 17.015 9.008)" />
+      </g>
+    </SvgIcon>
+  );
 };
