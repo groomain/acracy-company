68bc252c5fdfb4bc81f58b72cdf51aef1a2a942e
Sabine Robart <sabine.robart@protonmail.com>
2020-05-20 12:24:35 +0200

    Enable submitting the login form on pressing Enter

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { CustomButton } from '../Button/';
@@ -10,9 +10,8 @@ import styles from "./styles";
 
 const LoginForm = (props) => {
   const classes = styles();
-  const [open, setOpen] = React.useState(false);
 
-  const { loginErrorMessage, loginLoading } = useSelector(state => ({
+  const { loginLoading } = useSelector(state => ({
     loginErrorMessage: state.getIn(['app', 'loginErrorMessage']),
     loginLoading: state.getIn(['app', 'loginLoading'])
   }));
@@ -36,37 +35,38 @@ const LoginForm = (props) => {
       justify="center"
       className={classes.container}
     >
-      <CustomTextField
-        id="email"
-        type='email'
-        label={t('email') + '*'}
-        placeholder={t('yourEmail')}
-        value={email}
-        onBlur={handleBlur}
-        onChange={handleChange}
-        error={!!touched.email && !!errors.email}
-        helperText={touched.email && errors.email ? t(errors.email) : ''}
-      />
-      <CustomPasswordField
-        id="password"
-        label={t('password') + '*'}
-        placeholder={t('yourPassword')}
-        value={password}
-        onBlur={handleBlur}
-        onChange={handleChange}
-        error={!!touched.password && !!errors.password}
-        helperText={touched.password && errors.password ? t(errors.password) : ''}
-      />
-      <NavLink to={'/password'} className={classes.navLink}>{t('forgotPasswordButton')}</NavLink>
-      <Grid container direction={'row'} justify={'flex-end'}>
-        <CustomButton
-          type="submit"
-          handleClick={() => handleSubmit({ email, password })}
-          loading={loginLoading}
-          title={t('loginSubmit')}
-          theme="filledButton"
+      <form onSubmit={handleSubmit}>
+        <CustomTextField
+          id="email"
+          type='email'
+          label={t('email') + '*'}
+          placeholder={t('yourEmail')}
+          value={email}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          error={!!touched.email && !!errors.email}
+          helperText={touched.email && errors.email ? t(errors.email) : ''}
         />
-      </Grid>
+        <CustomPasswordField
+          id="password"
+          label={t('password') + '*'}
+          placeholder={t('yourPassword')}
+          value={password}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          error={!!touched.password && !!errors.password}
+          helperText={touched.password && errors.password ? t(errors.password) : ''}
+        />
+        <NavLink to={'/password'} className={classes.navLink}>{t('forgotPasswordButton')}</NavLink>
+        <Grid container direction={'row'} justify={'flex-end'}>
+          <CustomButton
+            type="submit"
+            loading={loginLoading}
+            title={t('loginSubmit')}
+            theme="filledButton"
+          />
+        </Grid>
+      </form>
     </Grid>
   );
 };
