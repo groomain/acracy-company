bd80f3478f7d10d31bda20dc5e811645966394b9
Emilie Boeglen <anaporia@protonmail.com>
2020-05-25 17:24:29 +0200

    [~] Remove fake progress bar.

--- src/components/SearchResultPannel/index.js
+++ src/components/SearchResultPannel/index.js
@@ -7,64 +7,61 @@ import CircleImage from '../CircleImage';
 import styles from './styles';
 
 const SearchResultPannel = (props) => {
-    const classes = styles();
-    const { t } = useTranslation();
+  const classes = styles();
+  const { t } = useTranslation();
 
-    const logos = [{ src: '' }, { src: '' }, { src: '' }, { src: '' }, { src: '' }, { src: '' }];
+  const logos = [{ src: '' }, { src: '' }, { src: '' }, { src: '' }, { src: '' }, { src: '' }];
 
-    // const queryString = windows.location.search;
-    // const urlParams = new URLSearchParams(queryString);
-    // const searchType = urlParams.get('searchType');
-    // const searchValue = urlParams.get('searchValue'); 
-    //                                             =>  TO BE USED INSTEAD OF THE CURRENT VALUE OF {searchValue} 
-    const searchValue = 'Social Media Strategist'; /// USED AS A PLACEHOLDER FOR NOW
+  // const queryString = windows.location.search;
+  // const urlParams = new URLSearchParams(queryString);
+  // const searchType = urlParams.get('searchType');
+  // const searchValue = urlParams.get('searchValue'); 
+  //                                             =>  TO BE USED INSTEAD OF THE CURRENT VALUE OF {searchValue} 
+  const searchValue = 'Social Media Strategist'; /// USED AS A PLACEHOLDER FOR NOW
 
-    return (
-        <Grid container direction="column"
-            justify='space-between' className={classes.pannel}
-        >
-            {searchValue && (
-                <>
-                    <Grid item>
-                        <Typography variant={"h1"}>
-                            {t('savedResearch')}
-                        </Typography>
-                    </Grid>
-                    <Grid item className={classes.researchGridItem} container>
-                        <Grid item>
-                            <SearchIcon className={classes.searchIcon} /> &nbsp;
+  return (
+    <Grid container direction="column"
+      justify='space-between' className={classes.pannel}
+    >
+      {searchValue && (
+        <>
+          <Grid item>
+            <Typography variant={"h1"}>
+              {t('savedResearch')}
+            </Typography>
+          </Grid>
+          <Grid item className={classes.researchGridItem} container>
+            <Grid item>
+              <SearchIcon className={classes.searchIcon} /> &nbsp;
                         </Grid>
-                        <Grid item>
-                            <Typography variant={"subtitle1"}>
-                                {searchValue}
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </>
-            )}
-            <Grid item className={classes.collaboratorsGridItem}>
-                <Typography variant={"h1"}>
-                    {t('collaborators')}
-                </Typography>
+            <Grid item>
+              <Typography variant={"subtitle1"}>
+                {searchValue}
+              </Typography>
             </Grid>
-            <br />
-            <Grid item >
-                <Grid container className={classes.iconContainer}>
-                    {logos.map((logo, i) => {
-                        return (
-                            < Grid key={i} item xs={4} >
-                                <CircleImage theme='partner' alt='partner' />
-                            </Grid>
-                        )
-                    })}
-                </Grid>
-                <Grid container style={{ marginTop: 40 }}>
-                    <Grid item xs={2} style={{ height: 2, backgroundColor: '#ecf805' }}></Grid>
-                    <Grid item xs={6} style={{ height: 1, backgroundColor: '#565e56' }}></Grid>
-                </Grid>
-            </Grid>
-        </Grid >
-    );
+          </Grid>
+        </>
+      )}
+      <Grid item className={classes.collaboratorsGridItem}>
+        <Typography variant={"h1"}>
+          {t('collaborators')}
+        </Typography>
+      </Grid>
+      <br />
+      <Grid item >
+        <Grid container className={classes.iconContainer}>
+          {logos.map((logo, i) => {
+            return (
+              < Grid key={i} item xs={4} >
+                <CircleImage theme='partner' alt='partner' />
+              </Grid>
+            )
+          })}
+        </Grid>
+
+      </Grid>
+    </Grid >
+  );
 }
 
 export default SearchResultPannel;
