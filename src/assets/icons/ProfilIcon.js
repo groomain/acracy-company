82b0f1124a042298f464bcbb70e4b17bc7d2e1ff
wkyubi <william.delbecq@lapilulerouge.io>
2020-05-27 09:57:08 +0200

    add ProfilIcon

--- src/assets/icons/ProfilIcon.js
+++ src/assets/icons/ProfilIcon.js
@@ -4,9 +4,7 @@ import SvgIcon from "@material-ui/core/SvgIcon";
 const ProfilIcon = ({ color, ...props }) => {
   return (
     <SvgIcon {...props} >
-        <g fill="none" fill-rule="evenodd" stroke-width="2" transform="translate(2 2)">
-            <path stroke={props.hovered ? '#ecf805' : '#FFF'} stroke-linejoin="round" d="M26 16c2.761 0 5 2.239 5 5 0 1.267-.471 2.423-1.247 3.304l-.163-.088C32.205 25.569 34 28.355 34 31.57c0 4.545-16 4.545-16 0 0-3.149 1.72-5.885 4.247-7.268C21.47 23.423 21 22.267 21 21c0-2.761 2.239-5 5-5z"/>
-        </g>
+        <path fill="none" fill-rule="evenodd" stroke={props.hovered ? '#ecf805' : '#FFF'} stroke-linejoin="round" stroke-width="2" d="M7.4 1c2.21 0 4 1.79 4 4 0 1.013-.377 1.938-.997 2.643l-.133-.071c2.094 1.082 3.53 3.311 3.53 5.885 0 3.636-12.8 3.636-12.8 0 0-2.52 1.376-4.709 3.398-5.815C3.777 6.938 3.4 6.012 3.4 5c0-2.21 1.79-4 4-4z"/>
     </SvgIcon>
   )
 };
