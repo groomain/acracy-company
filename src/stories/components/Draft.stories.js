c45a8457217514b4d67c95a95275bf931078545d
Sabine Robart <sabine.robart@protonmail.com>
2020-06-05 13:48:59 +0200

    Update the Drafts stories and create a mock file to match the API data structure

--- src/stories/components/Draft.stories.js
+++ src/stories/components/Draft.stories.js
@@ -3,77 +3,20 @@ import ProviderWrapper from '../../utils/Provider';
 import Draft from '../../components/Drafts/Draft';
 import Drafts from '../../components/Drafts/DraftsWrapper';
 
+import { leads } from '../mocks/leads';
+
 export default {
   title: 'Components|Drafts',
   component: Drafts
 };
 
 const styles = {
-  padding: '3rem'
+  padding: '3rem',
+  position: 'relative'
 }
 
-const drafts = [{
-  title: 'Status de brief',
-  progress: 30,
-  date: '22.04',
-  time: '10h12',
-  target: 'Web design',
-  new: true,
-  status: 'Start'
-}, {
-  title: 'Status de brief',
-  progress: 70,
-  date: '21.04',
-  time: '12h09',
-  content: 'Mon titre de brief très long sur deux lignes',
-  target: 'Web design',
-  new: false,
-  status: 'Pending'
-}, {
-  title: 'Status de brief',
-  progress: 30,
-  date: '22.04',
-  time: '10h12',
-  target: 'Web design',
-  new: true,
-  status: 'To Validate'
-}, {
-  title: 'Status de brief',
-  progress: 70,
-  date: '21.04',
-  time: '12h09',
-  content: 'Mon titre de brief très long sur deux lignes',
-  target: 'Web design',
-  new: false,
-  status: 'To Validate'
-}, {
-  title: 'Status de brief',
-  progress: 30,
-  date: '22.04',
-  time: '10h12',
-  target: 'Web design',
-  new: true,
-  status: 'To Validate'
-}, {
-  title: 'Status de brief',
-  progress: 70,
-  date: '21.04',
-  time: '12h09',
-  content: 'Mon titre de brief très long sur deux lignes',
-  target: 'Web design',
-  new: false,
-  status: 'To Validate'
-},];
 
-const draft = {
-  title: 'Status de brief',
-  progress: 30,
-  date: '21.04',
-  time: '12h09',
-  target: 'Web design',
-  new: true,
-  status: 'Pending'
-};
+const draft = leads[0];
 
 export const withDefault = () => (
   ProviderWrapper(
@@ -86,31 +29,14 @@ export const withDefault = () => (
 export const withList = () => (
   ProviderWrapper(
     <div style={styles}>
-      <Drafts drafts={drafts}>
+      <Drafts drafts={leads}>
         <Draft draft={draft} />
       </Drafts>
     </div>
   )
 );
 
-const twoDrafts = [{
-  title: 'Status de brief',
-  progress: 30,
-  date: '22.04',
-  time: '10h12',
-  target: 'Web design',
-  new: true,
-  status: 'Pending'
-}, {
-  title: 'Status de brief',
-  progress: 70,
-  date: '21.04',
-  time: '12h09',
-  content: 'Mon titre de brief très long sur deux lignes',
-  target: 'Web design',
-  new: false,
-  status: 'To Validate'
-}];
+const twoDrafts = leads.slice(0, 2);
 
 export const withLessThan3 = () => (
   ProviderWrapper(
