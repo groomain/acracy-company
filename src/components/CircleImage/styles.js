9259b18e7c714de586d5f4335db14e85fce4a77c
Sabine Robart <sabine.robart@protonmail.com>
2020-06-09 08:56:17 +0200

    Minor: Add icons + change the CircleImage background color

--- src/components/CircleImage/styles.js
+++ src/components/CircleImage/styles.js
@@ -2,24 +2,24 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 import { fade } from '@material-ui/core';
 
 export default makeStyles(theme => ({
-    icon: {
-        backgroundBlendMode: 'hue',
-        backgroundImage: 'linear-gradient(to bottom, #000000, #000000)',
-        filter: 'saturate(0)',
-    },
-    avatarLarge: {
-        width: '123px',
-        height: '122px',
-        backgroundColor: theme.palette.secondary.black
-    },
-    avatarSmall: {
-        width: '50px',
-        height: '50px',
-        backgroundColor: theme.palette.secondary.black
-    },
-    partner: {
-        width: 94,
-        height: 94,
-        background: `${fade(theme.palette.secondary.medium, 0.3)}`,
-    }
+  icon: {
+    backgroundBlendMode: 'hue',
+    backgroundImage: 'linear-gradient(to bottom, #000000, #000000)',
+    filter: 'saturate(0)',
+  },
+  avatarLarge: {
+    width: '123px',
+    height: '122px',
+    backgroundColor: theme.palette.secondary.mid
+  },
+  avatarSmall: {
+    width: '50px',
+    height: '50px',
+    backgroundColor: theme.palette.secondary.mid
+  },
+  partner: {
+    width: 94,
+    height: 94,
+    background: `${fade(theme.palette.secondary.medium, 0.3)}`,
+  }
 }));
