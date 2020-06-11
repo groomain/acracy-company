3031d60a47ae6c1d59c90ddb3d08eb0058712d8b
Sabine Robart <sabine.robart@protonmail.com>
2020-06-11 18:00:45 +0200

    Improve the Mission's side menu

--- src/components/Missions/Mission/LeftOverlay/index.js
+++ src/components/Missions/Mission/LeftOverlay/index.js
@@ -1,67 +1,60 @@
 import React from 'react';
 import styles from './styles';
-import Grid from "@material-ui/core/Grid";
+import { Grid } from "@material-ui/core";
 import { IncidentIcon } from "../../../../assets/icons/IncidentIcon";
 import { AdresseIcon } from "../../../../assets/icons/AdresseIcon";
 import { DownloadIcon } from "../../../../assets/icons/DownloadIcon";
-import { NavLink } from "react-router-dom";
 import { CloseIcon } from "../../../../assets/icons/CloseIcon";
-export const LeftOverlay = (props) => {
+
+export const LeftOverlay = ({ matching, mission, ...props }) => {
+
   const classes = styles();
-  const [hovered1, setOvered1] = React.useState(false);
-  const [hovered2, setOvered2] = React.useState(false);
-  const [hovered3, setOvered3] = React.useState(false);
-  const [hovered4, setOvered4] = React.useState(false);
-  const [hovered5, setOvered5] = React.useState(false);
-  const [hovered6, setOvered6] = React.useState(false);
+
+  const renderMenuLinks = () => {
+    const today = Date.now() / 1000;
+    if (matching) {
+      return (
+        <Grid item container direction={'row'} className={classes.row}>
+          <DownloadIcon />
+             Télécharger PDF
+        </Grid>
+      )
+    }
+    if (mission) {
+      return (
+        <>
+          <Grid item container direction={'row'} className={classes.row} onClick={() => console.log('toto')}>
+            <DownloadIcon />
+              Télécharger facture
+          </Grid>
+          <Grid item container direction={'row'} className={classes.row}>
+            <AdresseIcon />
+              Voir coordonées freelance
+          </Grid>
+          {mission?.brief.missionContext.startDate < today ? (
+            <>
+              <Grid item container direction={'row'} className={classes.row}>
+                <IncidentIcon />
+                  Déclarer un incident
+              </Grid>
+
+              <Grid item container direction={'row'} className={classes.row}>
+                <DownloadIcon />
+                  Télécharger facture
+              </Grid>
+            </>
+          ) : null}
+        </>
+      )
+    }
+  };
 
   return (
-    <Grid container item className={classes.container} direction={'column'} justify={'center'}>
-      <Grid item container direction={'row'} justify={"flex-end"}>
-        <CloseIcon hovered={hovered6} className={classes.closeIcon} onClick={() => props.setOpen(false)}
-          onMouseEnter={() => setOvered6(true)}
-          onMouseLeave={() => setOvered6(false)} />
+    <Grid container className={classes.container} direction={'column'}>
+      <Grid item container direction={'row'} justify={"flex-end"} className={classes.row}>
+        <CloseIcon className={classes.closeIcon} onClick={() => props.setOpen(false)} />
       </Grid>
-      <Grid item container direction={'row'} className={classes.row}>
-        <NavLink className={classes.navLink} to={'/'}
-          onMouseEnter={() => setOvered1(true)}
-          onMouseLeave={() => setOvered1(false)}>
-          <IncidentIcon hovered={hovered1} className={classes.icon} />
-                        Déclarer un incident
-                </NavLink>
-      </Grid>
-      <Grid item container direction={'row'} className={classes.row}>
-        <NavLink className={classes.navLink} to={'/'}
-          onMouseEnter={() => setOvered2(true)}
-          onMouseLeave={() => setOvered2(false)}>
-          <AdresseIcon hovered={hovered2} className={classes.icon} />
-                        Voir coordonées freelence
-                </NavLink>
-      </Grid>
-      <Grid item container direction={'row'} className={classes.row}>
-        <NavLink className={classes.navLink} to={'/'}
-          onMouseEnter={() => setOvered3(true)}
-          onMouseLeave={() => setOvered3(false)}>
-          <DownloadIcon hovered={hovered3} className={classes.icon} />
-                        Télécharger facture
-                </NavLink>
-      </Grid>
-      <Grid item container direction={'row'} className={classes.row}>
-        <NavLink className={classes.navLink} to={'/'}
-          onMouseEnter={() => setOvered4(true)}
-          onMouseLeave={() => setOvered4(false)}>
-          <DownloadIcon hovered={hovered4} className={classes.icon} />
-                        Télécharger facture
-                </NavLink>
-      </Grid>
-      <Grid item container direction={'row'} className={classes.row}>
-        <NavLink className={classes.navLink} to={'/'}
-          onMouseEnter={() => setOvered5(true)}
-          onMouseLeave={() => setOvered5(false)}>
-          <DownloadIcon hovered={hovered5} className={classes.icon} />
-                        Télécharger facture
-                </NavLink>
-      </Grid>
+      {renderMenuLinks()}
     </Grid>
   )
 };
--- src/components/Missions/Mission/LeftOverlay/styles.js
+++ src/components/Missions/Mission/LeftOverlay/styles.js
@@ -1,5 +1,4 @@
 import makeStyles from '@material-ui/core/styles/makeStyles';
-import { basierMedium } from "../../../../utils/configureMaterialTheme";
 
 export default makeStyles(theme => ({
   container: {
@@ -10,35 +9,25 @@ export default makeStyles(theme => ({
     right: '100%',
     borderRadius: '15px 0 0 15px',
     maxHeight: 323,
-    paddingLeft: 17
+    padding: '54px 0'
   },
   row: {
-    padding: 8,
-  },
-  navLink: {
-    display: 'flex',
-    paddingLeft: 25,
-    paddingRight: 25,
-    fontSize: 14,
-    color: '#fff',
-    textDecoration: 'none',
-    '&:hover': {
-      color: `${theme.palette.primary.main}`,
+    cursor: 'pointer',
+    padding: '8px 25px',
+    '&:hover svg g, &:hover svg path': {
+      stroke: theme.palette.primary.main
     },
-    '&:hover > g': {
-      color: `${theme.palette.primary.main}`,
+    '&:hover': {
+      color: theme.palette.primary.main
     }
-  },
-  icon: {
-    color: 'red'
   },
   closeIcon: {
     position: 'absolute',
     top: 20,
     right: 10,
-    '&:hover': {
-      cursor: 'pointer',
-      color: `${theme.palette.primary.main}`,
+    cursor: 'pointer',
+    '&:hover g': {
+      stroke: `${theme.palette.primary.main}`,
     },
   }
 }));
