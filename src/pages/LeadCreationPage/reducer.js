3eea6dc9ef16edcc603dc8b727c513b47cfa2fbd
Emilie Boeglen <anaporia@protonmail.com>
2020-06-23 19:27:34 +0200

    [~] Create sagas to update a lead, update a lead status, and get a lead data.

--- src/pages/LeadCreationPage/reducer.js
+++ src/pages/LeadCreationPage/reducer.js
@@ -5,12 +5,24 @@ const initialState = Immutable.Map({
   leadSaveLoading: false,
   leadSaveErrorMessage: false,
   leadSaveData: null,
-  leadDraftData: {
+  //
+  getLeadDraftLoading: false,
+  getLeadDraftErrorMessage: null,
+  LeadDraftId: null,
+  //
+  updateLeadDraftLoading: false,
+  updateLeadDraftErrorMessage: null,
+  updateResponse: null,
+  //
+  changeLeadStatusLoading: false,
+  changeLeadStatusErrorMessage: null,
+  changeResponse: null,
+  //
+  leadDraftSearchData: {
     search: null
   },
   deliverablesArray: [],
   dateFromCalendar: null,
-  missionTitle: '',
   dailyRate: 0.0
 });
 
@@ -19,24 +31,57 @@ const { actions, reducer } = createSlice({
   name: 'LeadCreation',
   initialState,
   reducers: {
+    // POST  /leads
     leadSaveLaunched: (state, action) => state
       .set('leadSaveLoading', true)
       .set('leadSaveErrorMessage', null),
     leadSaveSuccess: (state, action) => state
       .set('leadSaveLoading', false)
-      .set('leadSaveData', action.payload)
+      .set('leadSaveData', action.payload) // leadId
       .set('leadSaveErrorMessage', null),
     leadSaveFailure: (state, action) => state
       .set('leadSaveLoading', false)
       .set('leadSaveErrorMessage', action.payload),
-    setLeadDraft: (state, action) => state
-      .set('leadDraftData', action.payload),
+    // GET Draft  /leads/{leadId}
+    getLeadDraftLaunched: (state, action) => state
+      .set('getLeadDraftLoading', true)
+      .set('getLeadDraftErrorMessage', null),
+    getLeadDraftSuccess: (state, action) => state
+      .set('getLeadDraftLoading', false)
+      .set('getLeadDraftErrorMessage', null)
+      .set('LeadDraftId', action.payload),
+    getLeadDraftFailure: (state, action) => state
+      .set('getLeadDraftLoading', false)
+      .set('getLeadDraftErrorMessage', action.payload),
+    // PUT Draft  /leads/{leadId}
+    putLeadDraftLaunched: (state, action) => state
+      .set('updateLeadDraftLoading', true)
+      .set('updateLeadDraftErrorMessage', null),
+    putLeadDraftSuccess: (state, action) => state
+      .set('updateLeadDraftLoading', false)
+      .set('updateLeadDraftErrorMessage', null)
+      .set('updateResponse', action.payload),
+    putLeadDraftFailure: (state, action) => state
+      .set('updateLeadDraftLoading', false)
+      .set('updateLeadDraftErrorMessage', action.payload),
+    // POST status  /leads/{leadId}/actions
+    changeLeadStatusLaunched: (state, action) => state
+      .set('changeLeadStatusLoading', true)
+      .set('changeLeadStatusErrorMessage', null),
+    changeLeadStatusSuccess: (state, action) => state
+      .set('changeLeadStatusLoading', false)
+      .set('changeLeadStatusErrorMessage', null)
+      .set('changeResponse', action.payload),
+    changeLeadStatusFailure: (state, action) => state
+      .set('changeLeadStatusLoading', false)
+      .set('changeLeadStatusErrorMessage', action.payload),
+    // set separate infos from lead creation form
+    setLeadDraftSearchData: (state, action) => state
+      .set('leadDraftSearchData', action.payload),
     setDeliverablesArray: (state, action) => state
       .set('deliverablesArray', action.payload),
     setDateFromCalendar: (state, action) => state
       .set('dateFromCalendar', action.payload),
-    setMissionTitle: (state, action) => state
-      .set('missionTitle', action.payload),
     setDailyRate: (state, action) => state
       .set('dailyRate', action.payload)
   }
@@ -46,11 +91,23 @@ export const {
   leadSaveLaunched,
   leadSaveSuccess,
   leadSaveFailure,
-  setLeadDraft,
+  //
+  setLeadDraftSearchData,
   setDeliverablesArray,
   setDateFromCalendar,
-  setMissionTitle,
-  setDailyRate
+  //
+  putLeadDraftLaunched,
+  putLeadDraftSuccess,
+  putLeadDraftFailure,
+  //
+  changeLeadStatusLaunched,
+  changeLeadStatusSuccess,
+  changeLeadStatusFailure,
+  //
+  setDailyRate,
+  getLeadDraftLaunched,
+  getLeadDraftSuccess,
+  getLeadDraftFailure
 } = actions;
 
 export default reducer;
--- src/pages/LeadCreationPage/sagas.js
+++ src/pages/LeadCreationPage/sagas.js
@@ -1,28 +1,91 @@
 import { all, put, takeLatest } from 'redux-saga/effects';
 import { API } from 'aws-amplify';
+import { push } from 'connected-react-router';
 import { config } from '../../conf/amplify';
-import { leadSaveSuccess, leadSaveFailure } from "./reducer";
+import { leadSaveSuccess, leadSaveFailure, getLeadDraftSuccess, getLeadDraftFailure, changeLeadStatusSuccess, changeLeadStatusFailure } from "./reducer";
 
-function* doLeadSave(action) {
+function* doLeadSave(action) { // create a new lead
   // console.log('action: ', action.payload)
   try {
-
-    const leads = yield API.post(config.apiGateway.NAME, '/leads', {
+    const leadId = yield API.post(config.apiGateway.NAME, '/leads', {
       headers: {
         'x-api-key': config.apiKey
       },
       body: action.payload
     });
 
-    yield put(leadSaveSuccess(leads));
+    yield put(leadSaveSuccess(leadId));
+    yield put(push('/home'));
   } catch (error) {
     console.log(error);
     yield put(leadSaveFailure());
   }
 }
 
+function* doGetLeadDraft(action) { // get a lead's data
+  // console.log('action: ', action.payload)
+  const { id } = action.payload;
+  try {
+    const draft = yield API.get(config.apiGateway.NAME, encodeURI(`/leads/${id}`),
+      {
+        headers: {
+          'x-api-key': config.apiKey
+        }
+      });
+
+    yield put(getLeadDraftSuccess(draft));
+  } catch (error) {
+    console.log(error);
+    yield put(getLeadDraftFailure());
+  }
+}
+
+function* doUpdateLeadDraft(action) { // update an existing lead
+  // console.log('action: ', action.payload)
+  const { id, form } = action.payload;
+  try {
+    const draft = yield API.put(config.apiGateway.NAME, encodeURI(`/leads/${id}`),
+      {
+        headers: {
+          'x-api-key': config.apiKey
+        },
+        body: form
+
+      });
+
+    yield put(getLeadDraftSuccess(draft));
+    yield put(push('/home'));
+  } catch (error) {
+    console.log(error);
+    yield put(getLeadDraftFailure());
+  }
+}
+
+function* doChangeLeadStatus(action) {  // modify the status of a lead
+  // console.log('action: ', action.payload)
+  const { id, status } = action.payload;
+  try {
+    const update = yield API.post(config.apiGateway.NAME, encodeURI(`/leads/${id}/actions/`),
+      {
+        headers: {
+          'x-api-key': config.apiKey
+        },
+        body: { 'type': status }
+      });
+
+    yield put(changeLeadStatusSuccess(update));
+    yield put(push('/home'));
+  } catch (error) {
+    console.log(error);
+    yield put(changeLeadStatusFailure());
+  }
+}
+
 export default function* LeadCreationSaga() {
   yield all([
     takeLatest('LeadCreation/leadSaveLaunched', doLeadSave),
+    takeLatest('LeadCreation/getLeadDraftLaunched', doGetLeadDraft),
+    takeLatest('LeadCreation/putLeadDraftLaunched', doUpdateLeadDraft),
+    takeLatest('LeadCreation/changeLeadStatusLaunched', doChangeLeadStatus)
   ]);
 }
