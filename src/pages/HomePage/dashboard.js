99dee7d76000fc8c2c8eb6c584e726102c721e28
Sabine Robart <sabine.robart@protonmail.com>
2020-06-29 18:29:45 +0200

    Add redirection to the lead creation page after selecting a search value

--- src/pages/HomePage/dashboard.js
+++ src/pages/HomePage/dashboard.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from "react-i18next";
 import clsx from 'clsx';
+import { push } from "connected-react-router";
 
 import { Grid, Typography, Box } from '@material-ui/core/';
 
@@ -12,11 +13,16 @@ import sharedStyles from "../../utils/styles";
 
 export const Dashboard = () => {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
   const sharedClasses = sharedStyles();
 
   const { userName } = useSelector(state => ({
     userName: state.getIn(['app', 'userDynamo', 'employeeFirstName'])
   }))
+
+  const handleSearchResult = (e) => {
+    dispatch(push(e.TEXT ? `/lead?searchType=${e.TYPE}&searchCode=${e.objectID}&searchText=${e.TEXT}` : `/lead?searchText=${e.value}`));
+  }
 
   return (
     <Grid className={clsx(sharedClasses.pannelLayout, sharedClasses.fullPage)}>
@@ -26,7 +32,7 @@ export const Dashboard = () => {
           <Typography variant='body1'>{t('dashboard.subtitle')}</Typography>
         </Box>
         <Box my={2}>
-          <Searchbar />
+          <Searchbar onUpdateChosenCategory={handleSearchResult} />
         </Box>
       </Grid>
       <Box my={6}>
