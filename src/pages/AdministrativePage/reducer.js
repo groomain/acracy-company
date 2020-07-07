28c020fbc16f17222ebe5f9574c94d28cc53792f
Emilie Boeglen <anaporia@protonmail.com>
2020-07-07 19:30:25 +0200

    Create actions and sagas to get, upload and delete attachments.

--- src/pages/AdministrativePage/reducer.js
+++ src/pages/AdministrativePage/reducer.js
@@ -1,12 +1,23 @@
 import Immutable from 'immutable';
 import { createSlice } from "@reduxjs/toolkit";
+import { FormatListBulleted } from '@material-ui/icons';
 
 const initialState = Immutable.Map({
   companyLoading: false,
   companyData: null,
   companyError: null,
   companyUpdateLoading: false,
-  companyUpdateError: null
+  companyUpdateError: null,
+  compnayUpdateResponse: null,
+  getAttachmentsLoading: false,
+  getAttachmentsError: false,
+  attachments: [],
+  uploadAttachmentLoading: false,
+  uploadAttachmentError: null,
+  uploadResponse: null,
+  deleteAttachmentLoading: false,
+  deleteAttachmentError: null,
+  deleteResponse: null,
 });
 
 const { actions, reducer } = createSlice({
@@ -30,14 +41,51 @@ const { actions, reducer } = createSlice({
     putCompanyLaunched: (state, action) => state
       .set('companyUpdateLoading', true)
       .set('companyUpdateError', null)
-      .set('companyData', null),
+      .set('compnayUpdateResponse', null),
     putCompanySuccess: (state, action) => state
       .set('companyUpdateLoading', false)
-      .set('companyData', action.payload),
+      .set('compnayUpdateResponse', action.payload),
     putCompanyFailure: (state, action) => state
       .set('companyUpdateLoading', false)
-      .set('companyData', null)
+      .set('compnayUpdateResponse', null)
       .set('companyUpdateError', action.payload),
+    // GET attachments
+    getAttachmentsLaunched: (state, action) => state
+      .set('getAttachmentsLoading', true)
+      .set('getAttachmentsError', null)
+      .set('attachments', null),
+    getAttachmentsSuccess: (state, action) => state
+      .set('getAttachmentsLoading', false)
+      .set('attachments', action.payload),
+    getAttachmentsFailure: (state, action) => state
+      .set('getAttachmentsLoading', false)
+      .set('attachments', null)
+      .set('getAttachmentsError', action.payload),
+    // POST attachment
+    uploadAttachmentLaunched: (state, action) => state
+      .set('uploadAttachmentLoading', true)
+      .set('uploadAttachmentError', null)
+      .set('uploadResponse', null),
+    uploadAttachmentSuccess: (state, action) => state
+      .set('uploadAttachmentLoading', false)
+      .set('uploadResponse', action.payload),
+    uploadAttachmentFailure: (state, action) => state
+      .set('uploadAttachmentLoading', false)
+      .set('uploadResponse', null)
+      .set('uploadAttachmentError', action.payload),
+    // DELETE attachment
+    deleteAttachmentLaunched: (state, action) => state
+      .set('deleteAttachmentLoading', true)
+      .set('deleteAttachmentError', null)
+      .set('deleteResponse', null),
+    deleteAttachmentSuccess: (state, action) => state
+      .set('deleteAttachmentLoading', false)
+      .set('deleteResponse', action.payload),
+    deleteAttachmentFailure: (state, action) => state
+      .set('deleteAttachmentLoading', false)
+      .set('deleteResponse', null)
+      .set('deleteAttachmentError', action.payload),
+
   }
 });
 
@@ -47,7 +95,17 @@ export const {
   getCompanyFailure,
   putCompanyLaunched,
   putCompanySuccess,
-  putCompanyFailure
+  putCompanyFailure,
+  //////
+  getAttachmentsLaunched,
+  getAttachmentsSuccess,
+  getAttachmentsFailure,
+  uploadAttachmentLaunched,
+  uploadAttachmentSuccess,
+  uploadAttachmentFailure,
+  deleteAttachmentLaunched,
+  deleteAttachmentSuccess,
+  deleteAttachmentFailure
 } = actions;
 
 export default reducer;
--- src/pages/AdministrativePage/sagas.js
+++ src/pages/AdministrativePage/sagas.js
@@ -1,12 +1,16 @@
 import { all, put, takeLatest, delay } from 'redux-saga/effects';
 import { API } from 'aws-amplify';
-import { getCompanySuccess, getCompanyFailure, putCompanySuccess, putCompanyFailure } from './reducer';
+import {
+  getCompanySuccess, getCompanyFailure, putCompanySuccess, putCompanyFailure,
+  getAttachmentsSuccess, getAttachmentsFailure, uploadAttachmentSuccess, uploadAttachmentFailure, deleteAttachmentSuccess, deleteAttachmentFailure
+} from './reducer';
 import { config } from '../../conf/amplify';
 import { push } from "connected-react-router";
 import { openSnackBar } from "../../components/App/reducer";
 import companyMock from '../../mock/company'
 
 function* getCompany(action) {
+  // TODO récupérer l'id de l'entreprise
   try {
     let companyData = companyMock;
     // const companyData = yield API.get(config.apiGateway.NAME, `/companies/${id}`, {
@@ -26,7 +30,7 @@ function* getCompany(action) {
 }
 
 function* doUpdateCompany(action) {
-  console.log('action :', action);
+  console.log('action :', action.payload);
   try {
     let companyData = companyMock;
     //     const companyData = yield API.put(config.apiGateway.NAME, `/companies/${id}`, {
@@ -36,17 +40,67 @@ function* doUpdateCompany(action) {
     // body: action.payload
     // })
     yield put(putCompanySuccess(companyData))
-
+    yield put(openSnackBar({ message: "Vos données ont bien été enregistrées", error: false }));
   } catch (error) {
     yield put(putCompanyFailure())
     yield put(openSnackBar({ message: "Une erreur est survenue", error: true }));
   }
+}
 
+
+
+function* doGetAttachments() {
+  try {
+    const attachments = yield API.get(config.apiGateway.NAME, `/attachments`, {
+      header: {
+        'x-api-key': config.apiKey
+      },
+    })
+    yield put(getAttachmentsSuccess(attachments))
+  } catch (error) {
+    yield put(getAttachmentsFailure())
+    yield put(openSnackBar({ message: "Une erreur est survenue", error: true }));
+  }
+}
+
+function* doUploadAttachment() {
+  try {
+    const attachments = yield API.post(config.apiGateway.NAME, `/attachments`, {
+      header: {
+        'x-api-key': config.apiKey
+      },
+    })
+    yield put(uploadAttachmentSuccess(attachments))
+    yield put(openSnackBar({ message: "Votre document a bien été uploadé", error: false }));
+  } catch (error) {
+    yield put(uploadAttachmentFailure())
+    yield put(openSnackBar({ message: "Une erreur est survenue", error: true }));
+  }
+}
+
+function* doDeleteAttachment(action) {
+  const { id } = action.payload;
+  try {
+    const attachments = yield API.delete(config.apiGateway.NAME, `/attachments/${id}`, {
+      header: {
+        'x-api-key': config.apiKey
+      },
+    })
+    yield put(deleteAttachmentSuccess(attachments))
+    // utiliser le nom du doc dans la snackbar ??
+    yield put(openSnackBar({ message: "Votre document a bien été supprimé", error: false }));
+  } catch (error) {
+    yield put(deleteAttachmentFailure())
+    yield put(openSnackBar({ message: "Une erreur est survenue", error: true }));
+  }
 }
 
 export default function* administrativeSaga() {
   yield all([
     takeLatest('Administrative/getCompanyLaunched', getCompany),
-    takeLatest('Administrative/putCompanyLaunched', doUpdateCompany)
+    takeLatest('Administrative/putCompanyLaunched', doUpdateCompany),
+    takeLatest('Administrative/getAttachmentsLaunched', doGetAttachments),
+    takeLatest('Administrative/uploadAttachmentLaunched', doUploadAttachment),
+    takeLatest('Administrative/deleteAttachmentLaunched', doDeleteAttachment)
   ]);
 }
