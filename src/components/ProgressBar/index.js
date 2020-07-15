2d7f8fdc8928215d7c2832077225e019b67db476
Emilie Boeglen <anaporia@protonmail.com>
2020-07-15 15:23:12 +0200

    Add cases to the progress bar status.

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import styles from './styles'
 import { useSelector } from "react-redux";
 
-// utiliser les actions handleNextStep et handlePreviousStep de l'App reducer pour faire évoluer la bar
+// utiliser les actions handleCurrentStep de l'App reducer pour faire évoluer la bar
 
 const getWidth = (activeStep) => {
   switch (activeStep) {
     case 0:
       return { width: "0%" };
     case 1:
-      return { width: "10%" };
+      return { width: "15%" };
     case 2:
-      return { width: "58.33%" };
+      return { width: "45%" };
     case 3:
-      return { width: "100%" };
+      return { width: "58.33%" }; // => 100% of the left panel
+    case 4:
+      return { width: "100%" }; // not used for now
     default:
       return { width: "0%" };
   }
