cc08b17a77481724f49679a61a27ae0dea9a0203
Emilie Boeglen <anaporia@protonmail.com>
2020-07-16 09:58:42 +0200

    Add reasons array to the contact modal component.

--- src/components/ContactModale/index.js
+++ src/components/ContactModale/index.js
@@ -7,11 +7,11 @@ import IconButton from "@material-ui/core/IconButton";
 import CustomTextArea from "../Inputs/CustomTextArea";
 import CustomButton from "../Button";
 import CloseIcon from '@material-ui/icons/Close';
-import {contactAcracyLaunched} from "../../components/ContactModale/reducer";
+import { contactAcracyLaunched } from "../../components/ContactModale/reducer";
 import Grid from "@material-ui/core/Grid";
 import CustomSelect from "../Inputs/CustomSelect";
 
-const ContactModale = ({open, setOpen, interview, title, placeHolder, ...props }) => {
+const ContactModale = ({ open, setOpen, interview, title, placeHolder, ...props }) => {
   const classes = styles();
   const dispatch = useDispatch();
 
@@ -23,7 +23,7 @@ const ContactModale = ({open, setOpen, interview, title, placeHolder, ...props }
   const [contactMessage, setContactMessage] = useState('');
   const [contactSelect, setContactSelect] = useState('');
 
-  const contactAcracy = (message, reason,  interview) => {
+  const contactAcracy = (message, reason, interview) => {
     dispatch(contactAcracyLaunched({ message: message, reason: reason, interview: interview }));
     handleContactOpen()
   };
@@ -32,18 +32,20 @@ const ContactModale = ({open, setOpen, interview, title, placeHolder, ...props }
     setOpen(!open);
   };
 
+  const reasons = ["Question sur un brief", "Question/remarque sur une mission", "Question sur mon profil / l’administratif", "Question sur une facture", "Un souci ou une remarque sur le site", "Déclarer votre amour", "Autre chose"]
+
   return (
-      <Dialog open={open} onClose={handleContactOpen} classes={{ paper: classes.modale }} {...props}>
-        <Grid item container direction={'column'} justify={'center'} className={classes.modaleContainer}>
-          <IconButton aria-label="close" className={classes.iconButton} onClick={handleContactOpen}>
-            <CloseIcon />
-          </IconButton>
-          <Typography variant={"h1"}>{title}</Typography>
-          <CustomSelect placeholder={"Sélectionner raison"} label={"Raison"} optionsValues={['Contacter Acracy']} value={contactSelect} handleChangeOut={setContactSelect} />
-          <CustomTextArea placeholder={placeHolder} valueOut={contactMessage} handleChangeOut={setContactMessage} />
-          <CustomButton theme={"filledButton"} style={{ width: 254 }} title={"Envoyé"} handleClick={() => contactAcracy(contactMessage, contactSelect, interview)} loading={contactLoading} />
-        </Grid>
-      </Dialog>
+    <Dialog open={open} onClose={handleContactOpen} classes={{ paper: classes.modale }} {...props}>
+      <Grid item container direction={'column'} justify={'center'} className={classes.modaleContainer}>
+        <IconButton aria-label="close" className={classes.iconButton} onClick={handleContactOpen}>
+          <CloseIcon />
+        </IconButton>
+        <Typography variant={"h1"}>{title}</Typography>
+        <CustomSelect placeholder={"Sélectionner raison"} label={"Raison"} optionsValues={reasons} value={contactSelect} handleChangeOut={setContactSelect} />
+        <CustomTextArea placeholder={placeHolder} valueOut={contactMessage} handleChangeOut={setContactMessage} />
+        <CustomButton theme={"filledButton"} style={{ width: 254 }} title={"Envoyer"} handleClick={() => contactAcracy(contactMessage, contactSelect, interview)} loading={contactLoading} />
+      </Grid>
+    </Dialog>
   );
 };
 
