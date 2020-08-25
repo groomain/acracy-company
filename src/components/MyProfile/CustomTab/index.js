3591598ff72dc94702d10c5e480e09c802ecdc06
Sabinerobart <sabine.robart@protonmail.com>
2020-08-25 18:04:12 +0200

    Update the profile page's tabs styling

--- src/components/MyProfile/CustomTab/index.js
+++ src/components/MyProfile/CustomTab/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import Tab from '@material-ui/core/Tab';
 import styles from './styles';
 import { Link } from "react-scroll/modules";
+import clsx from "clsx";
 
-const CustomTab = ({ label, missingInfos, to, setActive, ...props }) => {
+const CustomTab = ({ label, missingInfos, to, setActive, selected, ...props }) => {
   const classes = styles();
 
   return (
     <Link to={to} smooth={true} spy={true} onSetActive={(to) => { setActive(to) }}>
       <Tab className={classes.underSecondaryTitle} variant="body2" label={label}
         classes={{
-          root: classes.root,
+          root: clsx(classes.root, { [classes.selected]: selected }),
           wrapper: classes.wrapper,
-          selected: classes.selected
         }}
         {...props}
       />
