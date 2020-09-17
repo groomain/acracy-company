6f439b003d9c30c2696c89ac9e4a3b7c44004016
Sabinerobart <sabine.robart@protonmail.com>
2020-09-17 18:41:51 +0200

    Improve draft checks

--- src/components/Drafts/Draft/index.js
+++ src/components/Drafts/Draft/index.js
@@ -56,7 +56,7 @@ const Draft = ({ draft }) => {
     const getStatus = (draftStatus) => {
       let status;
       if (draftStatus === 'DRAFT') {
-        if (draft?.missionDetail || !checkNoExpertisesOrLanguages()) {
+        if (draft?.missionDetail || (draft?.missionRequirements && !checkNoExpertisesOrLanguages())) {
           return status = {
             title: FINALIZE_BRIEF,
             progress: 80
@@ -69,7 +69,7 @@ const Draft = ({ draft }) => {
           }
         }
       } else if (draftStatus === 'HELP_NEEDED' || checkNoExpertisesOrLanguages()) {
-        if (!draft?.missionDetail || !draft?.missionRequirements) {
+        if (!draft?.missionDetail || !draft?.missionRequirements || (draft?.missionRequirements && checkNoExpertisesOrLanguages())) {
           return status = {
             title: GET_CALLED,
             progress: 40,
