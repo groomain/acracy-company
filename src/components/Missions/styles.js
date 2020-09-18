876a256c61a15fd3d124cdfbe3b8e2e108e01891
Lionel Ipser <lionel@lapilulerouge.io>
2020-09-18 14:18:57 +0200

    fix(ACR-695) : change 'no mission' style

---
+++ src/components/Missions/styles.js
@@ -0,0 +1,18 @@
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+export default makeStyles(theme => ({
+    noMissionTitle: {
+        color: theme.palette.secondary.medium,
+        fontFamily: 'Basier Medium',
+        fontSize: '17px',
+        lineHeight: '29px',
+        letterSpacing: '-0.45px'
+    },
+    noMissionSubtitle: {
+        color: theme.palette.secondary.medium,
+        fontFamily: 'Basier Regular',
+        fontSize: '17px',
+        lineHeight: '29px',
+        letterSpacing: '-0.45px'
+    }
+}));
