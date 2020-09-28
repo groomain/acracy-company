c194c995b77b83608546ca19c935b81fce6a8deb
loumiamaryline <malves.16@laposte.net>
2020-09-28 16:22:36 +0200

    Displays profiles, and empty field if modification of the selection

--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -22,7 +22,7 @@ import profilIconYellow from '../../assets/icons/profil-roll-out-yellow.svg';
 import livrableYellow from '../../assets/icons/livrable-yellow.svg';
 
 const CustomConfigure = connectStateResults(({ searchResults }) =>
-  <Configure hitsPerPage={Math.max(1000, searchResults?.nbHits)} />
+  <Configure hitsPerPage={searchResults?.nbHits || 1000} />
 );
 
 const Searchbar = ({ onUpdateChosenCategory, value, hideLabel }) => {
